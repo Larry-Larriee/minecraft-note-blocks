2a7845210ced2e9dcc79375d444c4ec07efc20ff
Larry-Larriee <lle114@student.hbuhsd.edu>
2023-07-09 23:13:31 -0700

    Major Changes
    Click plays sound & ability to swap sounds for the note block

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
+import React, { useState, useEffect, useRef } from 'react';
 import noteBlock from "./note-block.png";
 import arcArrow from "./Arc-Arrow.png";
 import './App.css';
+import { banjo, bass, bassDrum, bell, bit, chime, cowBell, didgeridoo, ePiano, flute, guitar, hat, ironXylophone, snare, xylophone } from "./sounds";
 
 function App() {
+
+  const noteBlockRef = useRef(null);
+  const switchSoundButton = useRef(null);
+
+  let [soundQueue, changeQueue] = useState(0);
+
+  useEffect(() => {
+    const sounds = [banjo, bass, bassDrum, bell, bit, chime, cowBell, didgeridoo, ePiano, flute, guitar, hat, ironXylophone, snare, xylophone];
+
+    // Play the current music block sound
+    const playSound = () => {
+      const soundToPlay = sounds[soundQueue];
+      soundToPlay.play();
+    }
+
+    // Switch the current music block sound
+    const switchSound = () => {
+      if (soundQueue === (sounds.length - 1)) alterQueue();
+      return sounds[soundQueue++];
+    }
+
+    // Helper for switchSound
+    const alterQueue = () => {
+      changeQueue((previous) => {
+        if (previous === (sounds.length - 1)) return -1;
+      })
+    }
+
+    const noteBlock = noteBlockRef.current;
+    const switchSoundButt = switchSoundButton.current;
+
+    noteBlock.addEventListener("click", playSound);
+    switchSoundButt.addEventListener("click", switchSound);
+
+    return () => {
+      noteBlock.removeEventListener("click", playSound);
+      switchSoundButt.removeEventListener("click", switchSound);
+    }
+  }, [soundQueue])
+
   return (
     <div className="App">
       <nav>
         <ul>
           <li><p>MC Note Blocks</p></li>
-          <li><a href="/" onClick={""}>Switch Block</a></li>
+          <li><h3 ref={switchSoundButton} className='no-select'>Switch Sound</h3></li>
         </ul>
       </nav>
 
@@ -18,7 +60,7 @@ function App() {
         </section>
 
         <section className="note-block-image">
-          <img src={noteBlock} alt="Note Block" />
+          <img src={noteBlock} alt="Note Block" draggable="false" ref={noteBlockRef} className='no-select' />
         </section>
 
         <section className="note-block-material">
@@ -27,7 +69,7 @@ function App() {
 
         <p className="note-block-material-text">Material:</p>
         <div className="note-block-material-arrow">
-          <img src={arcArrow} alt="Arc Arrow" />
+          <img src={arcArrow} alt="Arc Arrow" draggable="false" className='no-select' />
         </div>
       </div>
     </div>
