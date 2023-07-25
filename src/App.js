bc5c776cb8be736ead73c9387a1ca286413f3341
Larry-Larriee <lle114@student.hbuhsd.edu>
2023-07-25 00:56:46 -0700

    Dropbox GUI
    Dropbox will display all note block sounds when clicked and can be unclicked. Created using position relative and absolute

--- src/App.js
+++ src/App.js
@@ -16,6 +16,8 @@ import noteBlue3 from "./Note-Block-Notes/note-blue-3.png";
 
 function App() {
 
+  const dropSelectReference = useRef(null);
+
   const soundReference = useRef(null);
   const soundMaterialReference = useRef(null);
   const note1 = useRef(null);
@@ -68,6 +70,12 @@ function App() {
       }
     }
 
+    // Select the note block sound from the dropbox
+    const selectSound = (number) => {
+      changeQueue(number);
+      changeSoundName();
+    }
+
     // Display the name of sounds
     const changeSoundName = () => {
       const soundName = newSoundNames[soundQueue];
@@ -80,11 +88,11 @@ function App() {
     // Animate notes for note block
     const animateNotes = (number) => {
       notes[number].current.classList.add("rise-up");
-      notes[number].current.classList.remove("content-hidden");
+      notes[number].current.classList.remove("content-gone");
 
       setTimeout(() => {
         notes[number].current.classList.remove("rise-up");
-        notes[number].current.classList.add("content-hidden");
+        notes[number].current.classList.add("content-gone");
       }, 1250);
     }
 
@@ -100,12 +108,43 @@ function App() {
     }
   }, [soundQueue])
 
+  const openDropSelect = () => {
+    const dropSelect = dropSelectReference.current;
+    dropSelect.classList.toggle("content-hidden");
+  }
+
   return (
     <div className="App">
       <nav>
         <ul>
           <li><p>MC Note Blocks</p></li>
-          <li><h3 ref={switchSoundButton} className='no-select'>Switch Sound</h3></li>
+
+          <section>
+            <li><h3 ref={switchSoundButton} className='no-select'>Switch Sound</h3></li>
+            <li>
+              <div className="drop-select">
+                <h3 onClick={openDropSelect}>Drop Select</h3>
+                <article ref={dropSelectReference} className="drop-select-open content-hidden">
+                  <p>Banjo</p>
+                  <p>Bass Drum</p>
+                  <p>Bass</p>
+                  <p>Bells</p>
+                  <p>"Bit"</p>
+                  <p>Chimes</p>
+                  <p>Cow Bell</p>
+                  <p>Didgeridoo</p>
+                  <p>"Pling"</p>
+                  <p>Flute</p>
+                  <p>Guitar</p>
+                  <p>Clicks and Sticks</p>
+                  <p>Iron Xylophone</p>
+                  <p>Snare Drum</p>
+                  <p>Xylophone</p>
+                </article>
+              </div>
+            </li>
+          </section>
+
         </ul>
       </nav>
 
@@ -123,16 +162,16 @@ function App() {
         </section>
 
         <section className="note-block-notes">
-          <img ref={note1} src={noteRed1} alt="Note" draggable="false" className='no-select content-hidden' />
-          <img ref={note2} src={noteOrange1} alt="Note" draggable="false" className='no-select content-hidden' />
-          <img ref={note3} src={noteOrange2} alt="Note" draggable="false" className='no-select content-hidden' />
-          <img ref={note4} src={noteBlue1} alt="Note" draggable="false" className='no-select content-hidden' />
-          <img ref={note5} src={noteBlue2} alt="Note" draggable="false" className='no-select content-hidden' />
-          <img ref={note6} src={noteBlue3} alt="Note" draggable="false" className='no-select content-hidden' />
-          <img ref={note7} src={noteGreen1} alt="Note" draggable="false" className='no-select content-hidden' />
-          <img ref={note8} src={noteGreen2} alt="Note" draggable="false" className='no-select content-hidden' />
-          <img ref={note9} src={noteGreen3} alt="Note" draggable="false" className='no-select content-hidden' />
-          <img ref={note10} src={notePurple1} alt="Note" draggable="false" className='no-select content-hidden' />
+          <img ref={note1} src={noteRed1} alt="Note" draggable="false" className='no-select content-gone' />
+          <img ref={note2} src={noteOrange1} alt="Note" draggable="false" className='no-select content-gone' />
+          <img ref={note3} src={noteOrange2} alt="Note" draggable="false" className='no-select content-gone' />
+          <img ref={note4} src={noteBlue1} alt="Note" draggable="false" className='no-select content-gone' />
+          <img ref={note5} src={noteBlue2} alt="Note" draggable="false" className='no-select content-gone' />
+          <img ref={note6} src={noteBlue3} alt="Note" draggable="false" className='no-select content-gone' />
+          <img ref={note7} src={noteGreen1} alt="Note" draggable="false" className='no-select content-gone' />
+          <img ref={note8} src={noteGreen2} alt="Note" draggable="false" className='no-select content-gone' />
+          <img ref={note9} src={noteGreen3} alt="Note" draggable="false" className='no-select content-gone' />
+          <img ref={note10} src={notePurple1} alt="Note" draggable="false" className='no-select content-gone' />
         </section>
 
       </div>
